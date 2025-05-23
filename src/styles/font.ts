00cb2199c9808db1c657459f3ec8871a442f6868
John Kyle Lampa <jklampa31@gmail.com>
2025-05-23 11:26:01 +0800

    chore: added fonts and colors to globals.css

---
+++ src/styles/font.ts
@@ -0,0 +1,13 @@
+import { Inter, Instrument_Sans } from 'next/font/google';
+
+export const inter = Inter({
+  variable: '--font-inter',
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+});
+
+export const instrument_sans = Instrument_Sans({
+  variable: '--font-instrument-sans',
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+});
