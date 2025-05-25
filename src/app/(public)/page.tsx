deb3fb845454167a951f82f694498cf847192a90
John Lampa <jklampa31@gmail.com>
2025-05-25 17:04:15 +0800

    chore: added favicon and changed title of home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,36 @@
+import Head from 'next/head';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SAMAHAN Systems Development",
+  description: "The official website of SAMAHAN Systems Development",
+};
+
 export default function Home() {
-  return <div>{"Insert your component here"}</div>;
+  return (<>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico?v=M44lzPylqQ" />
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/images/safari-pinned-tab.svg?v=M44lzPylqQ"
+          color="#000000"
+        ></link>
+      </Head>
+      <main>
+        {"insert your component here"}
+      </main>
+    </>);
 }
