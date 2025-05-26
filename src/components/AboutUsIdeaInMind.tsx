f72c91c7187704d690018158f5bbe421f44bf6fb
John Lampa <jklampa31@gmail.com>
2025-05-26 13:24:51 +0800

    fix: edits to component

--- src/components/AboutUsIdeaInMind.tsx
+++ src/components/AboutUsIdeaInMind.tsx
@@ -4,21 +4,17 @@ import { useRouter } from "next/navigation";
 
 import Button from "./ui/Button";
 
-export default function AboutUsProjects() {
+export default function AboutUsIdea() {
   const router = useRouter();
-
-  const handleClick = () => {
-    router.push("/");
-  };
 
   return (
       <div className="w-full gap-3 flex flex-col items-center py-24 text-blue3">
-        <div className="text-6xl  font-instrument-sans">Have a project or idea in mind? </div>
-        <div className="text-6xl font-semibold font-instrument-sans">
+        <div className="text-5xl  font-instrument-sans">Have a project or idea in mind? </div>
+        <div className="text-5xl font-semibold font-instrument-sans">
           Let’s build it together
         </div>
-        <div className="flex items-center h-14 mt-5">
-          <Button className="font-bold w-[700px] " onClick={handleClick}>
+        <div className="flex items-center h-14 mt-3">
+          <Button className="font-bold w-[550px] " onClick={() => router.push("/")}>
             Get in Touch
           </Button>
         </div>
