5b232191da8bc3aa2f38aa994b65235dac0ab98d
John Lampa <jklampa31@gmail.com>
2025-05-27 17:04:28 +0800

    fix: added members.json for data and member type

---
+++ src/lib/types/members.ts
@@ -0,0 +1,9 @@
+export type Member = {
+    name: string;
+    email: string;
+    position: string;
+    position2?: string;
+    position3?: string;
+    position4?: string;
+    imageUrl?: string;
+}
