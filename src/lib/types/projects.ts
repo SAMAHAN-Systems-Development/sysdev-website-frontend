e5fe06eb4934e5a61e3ee614e0217e9b7c2d2c7c
John Lampa <jklampa31@gmail.com>
2025-05-28 01:48:34 +0800

    refactor: added description and members array to project type

--- src/lib/types/projects.ts
+++ src/lib/types/projects.ts
@@ -1,9 +1,11 @@
 export type Project = {
     imageUrl: string;
     projectName: string;
+    description: string;
     websiteUrl: string;
     githubUrl: string;
     clientName: string;
     deploymentMonth: string;
     deploymentYear: number;
+    members?: { name: string; category: string; title: string }[];
 };
