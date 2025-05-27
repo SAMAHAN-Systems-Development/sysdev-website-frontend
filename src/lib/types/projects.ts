d5e7d290dff92a98308a94027f001930c1b7bea1
John Lampa <jklampa31@gmail.com>
2025-05-27 14:18:47 +0800

    feat: added project type

---
+++ src/lib/types/projects.ts
@@ -0,0 +1,9 @@
+export type Project = {
+    imageUrl: string;
+    projectName: string;
+    websiteUrl: string;
+    githubUrl: string;
+    clientName: string;
+    deploymentMonth: string;
+    deploymentYear: number;
+};
