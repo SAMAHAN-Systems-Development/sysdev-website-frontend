b9fa3eeb9499af43a5c6725536dfe45498db1fc4
John Lampa <jklampa31@gmail.com>
2025-06-02 12:17:26 +0800

    fix: scaling

--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -17,8 +17,8 @@ const servicesData = importedServicesData as ServiceItem[];
 
 const Services = () => {
     return (
-        <div className="w-full md:max-w-4xl lg:max-w-5xl mx-auto px-4 sm:px-8 md:px-12 lg:px-20 py-8">
-            <div className="grid grid-cols-2 lg:grid-cols-3 gap-0">
+        <div className="w-full max-w-96 sm:max-w-lg md:max-w-2xl lg:max-w-5xl mx-auto px-8 md:px-12 lg:px-20 py-8">
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-y-4 gap-x-0 sm:gap-x-4 lg:gap-0">
                 {servicesData.map((item: ServiceItem) => (
                     <div
                         key={item.id}
@@ -31,13 +31,13 @@ const Services = () => {
                     >
                         {item.type === 'text' ? (
                             <div className="font-instrument-sans bg-white p-3 sm:p-4 md:p-6 flex flex-col justify-center text-center h-full">
-                                <h3 className="text-sm sm:text-base md:text-lg font-bold text-blue3 mb-1 sm:mb-2">{item.title}</h3>
+                                <h3 className="text-[11px] sm:text-base md:text-lg font-bold text-blue3 mb-1 sm:mb-2">{item.title}</h3>
                                 <p className="text-[10px] sm:text-xs md:text-sm text-blue2 leading-snug sm:leading-relaxed">
                                     {item.description}
                                 </p>
                             </div>
                         ) : (
-                            <div className="relative w-full h-full min-h-[120px] sm:min-h-[150px] md:min-h-[180px] lg:min-h-[200px]">
+                            <div className="relative w-full h-full min-h-[120px] sm:min-h-[150px] md:min-h-[230px] lg:min-h-[200px]">
                                 <Image
                                     src={item.src || ''}
                                     alt={item.alt || 'Service Image'}
