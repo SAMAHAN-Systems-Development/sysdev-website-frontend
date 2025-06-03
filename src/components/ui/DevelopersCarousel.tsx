fd44f09232c8ee00a58666fde44c25c3c2c7aa9b
John Lampa <jklampa31@gmail.com>
2025-06-03 22:41:11 +0800

    fix: scaling

--- src/components/ui/DevelopersCarousel.tsx
+++ src/components/ui/DevelopersCarousel.tsx
@@ -45,7 +45,7 @@ const DevelopersCarousel = () => {
 
   return (
     <div className="w-full py-5 flex justify-center items-center">
-      <div className="relative w-11/12  h-[600px] rounded-4xl overflow-hidden">
+      <div className="relative w-full md:w-5/6 md:max-w-4xl h-80 md:h-[450px] lg:h-[500px] md:rounded-3xl lg:rounded-4xl overflow-hidden">
         {/* Slides */}
         <div
           ref={slideRef}
@@ -68,10 +68,10 @@ const DevelopersCarousel = () => {
         {/* Overlay content */}
         <div className="absolute inset-0 bg-blue-950/50 flex justify-center items-center z-10">
           <div className="flex flex-col items-center text-yellow2">
-            <h1 className="text-3xl md:text-6xl font-bold">
+            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-1 md:mb-3">
               {carouselItems[currentIndex].title}
             </h1>
-            <p className="text-xl md:text-4xl text-center max-w-xl text-white">
+            <p className="text-sm sm:text-lg md:text-xl lg:text-2xl text-center max-w-48 sm:max-w-72 md:max-w-xl text-white">
               {carouselItems[currentIndex].desc}
             </p>
           </div>
@@ -80,33 +80,33 @@ const DevelopersCarousel = () => {
         {/* Navigation Buttons */}
         <button
           onClick={prevSlide}
-          className="absolute left-6 top-1/2 transform -translate-y-1/2 z-20 text-white  hover:text-[#FFDF36] active:text-[#F8D000] cursor-pointer transition-all"
+          className="absolute left-4 md:left-6 top-1/2 transform -translate-y-1/2 z-20 text-white  hover:text-[#FFDF36] active:text-[#F8D000] cursor-pointer transition-all"
         >
           <span className="hidden sm:block">
             <FaArrowCircleLeft size={30}></FaArrowCircleLeft>
           </span>
           <span className="block sm:hidden">
-            <FaArrowLeft size={24}></FaArrowLeft>
+            <FaArrowLeft size={16}></FaArrowLeft>
           </span>
         </button>
         <button
           onClick={nextSlide}
-          className="absolute right-6 top-1/2 transform -translate-y-1/2 z-20 text-white  hover:text-[#FFDF36] active:text-[#F8D000] cursor-pointer transition-all"
+          className="absolute right-4 md:right-6 top-1/2 transform -translate-y-1/2 z-20 text-white  hover:text-[#FFDF36] active:text-[#F8D000] cursor-pointer transition-all"
         >
           <span className="hidden sm:block">
             <FaArrowCircleRight size={30}></FaArrowCircleRight>
           </span>
           <span className="block sm:hidden">
-            <FaArrowRight size={24}></FaArrowRight>
+            <FaArrowRight size={16}></FaArrowRight>
           </span>
         </button>
         {/* Pagination Dots */}
-        <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-20 flex gap-2">
+        <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-20 flex gap-1.5 md:gap-2">
           {carouselItems.map((_, index) => (
             <button
               key={index}
               onClick={() => setCurrentIndex(index)}
-              className={`w-3 h-3 rounded-full transition-all duration-300 cursor-pointer ${
+              className={`w-2 h-2 sm:w-2.5 sm:h-2.5 md:w-3 md:h-3 rounded-full transition-all duration-300 cursor-pointer ${
                 index === currentIndex
                   ? "bg-[#FFDF36] scale-125"
                   : "bg-white/50"
