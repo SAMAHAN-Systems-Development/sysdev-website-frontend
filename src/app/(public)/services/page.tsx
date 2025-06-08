cf761d176cc003894c5ac1843e5bb7a2509f6fef
John Lampa <jklampa31@gmail.com>
2025-06-08 21:23:45 +0800

    chore: assemble services page

---
+++ src/app/(public)/services/page.tsx
@@ -0,0 +1,10 @@
+import ServicesPage from '@/components/pages/ServicesPage'
+import React from 'react'
+
+function Page() {
+  return (
+    <div><ServicesPage/></div>
+  )
+}
+
+export default Page
---
+++ src/components/pages/ServicesPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ServiceHeroBanner from '../ui/ServiceHeroBanner'
+import WhatWeOfferSection from '../sections/WhatWeOfferSection'
+
+function ServicesPage() {
+  return (
+    <div>
+        <ServiceHeroBanner />
+        <WhatWeOfferSection />
+    </div>
+  )
+}
+
+export default ServicesPage
