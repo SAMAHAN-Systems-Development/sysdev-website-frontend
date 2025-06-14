4f6d51758d53032ccac63642d97874bd78836804
John Lampa <jklampa31@gmail.com>
2025-06-15 01:49:52 +0800

    chore: finalized services page

--- src/components/sections/WhatWeOfferSection.tsx
+++ src/components/sections/WhatWeOfferSection.tsx
@@ -5,8 +5,8 @@ import Services from "../ui/Services";
 export default function WhatWeOfferSection() {
 
   return (
-    <section className="py-10 px-2 sm:py-12 sm:px-4 md:py-16 md:px-8 max-w-6xl mx-auto flex flex-col items-center">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mt-20 mb-3 md:mt-4 md:mb-4 text-[var(--color-blue3)]">
+    <section className="px-2 sm:px-4 md:px-8 max-w-6xl mx-auto flex flex-col items-center mt-10 md:mt-16">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-3 md:mb-4 text-[var(--color-blue3)]">
         WHAT <span className="text-[var(--color-yellow2)]">WE</span> OFFER
       </h1>
       <div className="px-8 md:px-12 lg:px-20 w-full max-w-96 sm:max-w-lg md:max-w-2xl lg:max-w-5xl mx-auto mb-10 md:mb-16">
@@ -14,7 +14,6 @@ export default function WhatWeOfferSection() {
       </div>
       
       <Services/>
-      <div className="h-13"></div>
       <ServiceProjectsIdeaInMind />
 
     </section>
--- src/components/ui/ServiceProjectsIdeaInMind.tsx
+++ src/components/ui/ServiceProjectsIdeaInMind.tsx
@@ -3,11 +3,11 @@ import Button from './Button';
 
 const ServiceProjectsIdeaInMind = () => {
   return (
-    <div className="max-w-96 sm:max-w-lg md:max-w-2xl lg:max-w-5xl px-8 md:px-12 lg:px-20 py-4 mx-auto h-48 sm:h-56 md:h-72 lg:h-96 flex items-center">
+    <div className="max-w-96 sm:max-w-lg md:max-w-2xl lg:max-w-5xl px-8 md:px-12 lg:px-20 py-4 mx-auto h-48 sm:h-56 md:h-72 lg:h-96 flex items-center my-12 md:my-20">
       <div className="rounded-2xl overflow-hidden shadow-lg flex h-full">
         {/* Text Content - Responsive width */}
         <div className="w-1/2 sm:w-2/5 lg:w-2/5 px-4 py-7 sm:px-6 md:px-10 flex flex-col justify-center z-10 bg-blue3">
-          <h2 className="text-sm sm:text-lg md:text-2xl lg:text-3xl text-white mb-3 lg:mb-5 leading-tight">
+          <h2 className="text-xs sm:text-lg md:text-xl lg:text-2xl text-white mb-3 lg:mb-5 leading-tight">
             Have a project or idea in mind? Let&apos;s build it together
           </h2>
           <div className='flex items-center min-h-8 md:min-h-11 lg:min-h-12'>
