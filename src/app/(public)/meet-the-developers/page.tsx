e33d3da9a79ed927ea8c02aeffd14e4451d7f5ee
John Lampa <jklampa31@gmail.com>
2025-06-18 02:28:33 +0800

    feat: added meet the developers page

---
+++ src/app/(public)/meet-the-developers/page.tsx
@@ -0,0 +1,12 @@
+import MeetTheDevsPage from '@/components/pages/MeetTheDevsPage'
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+        <MeetTheDevsPage/>
+    </div>
+  )
+}
+
+export default Page
