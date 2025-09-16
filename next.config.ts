70cee72ac06a289ecf31482e28067341aaf6b153
John Lampa <jklampa31@gmail.com>
2025-09-17 00:52:59 +0800

    fix: added remote patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "cdn.jsdelivr.net", "103.37.49.230"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "samahan.addu.edu.ph",
+        pathname: "/sysdev/minio/sysdev-images/**",
+      },
+    ],
+    domains: [
+      "avatars.githubusercontent.com",
+      "cdn.jsdelivr.net",
+      "103.37.49.230",
+    ],
   },
   publicRuntimeConfig: {
     basePath: "", // Set your basePath value here
