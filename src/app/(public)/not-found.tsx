160bd4716fb7f2fd7319390bc55e380a694b570b
Ram Christian D. Nacar <139614619+remagII@users.noreply.github.com>
2025-10-08 19:10:42 +0800

    fix: remove min-height

--- src/app/(public)/not-found.tsx
+++ src/app/(public)/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="bg-gradient-to-b from-slate-950 to-blue3 flex flex-col items-center justify-center min-h-[calc(100vh-200px)] px-4 py-16 font-inter">
+    <div className="bg-gradient-to-b from-slate-950 to-blue3 flex flex-col items-center justify-center px-4 py-16 font-inter">
       <h1 className="text-[128px] md:text-[256px] text-yellow4 font-extrabold">
         404
       </h1>
