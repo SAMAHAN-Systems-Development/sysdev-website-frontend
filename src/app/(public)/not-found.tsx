50df1d09935d95739db49ac31706620ffb94fb0f
Ram Christian D. Nacar <139614619+remagII@users.noreply.github.com>
2025-10-08 19:04:10 +0800

    feat: Added Custom 404 Page

---
+++ src/app/(public)/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="bg-gradient-to-b from-slate-950 to-blue3 flex flex-col items-center justify-center min-h-[calc(100vh-200px)] px-4 py-16 font-inter">
+      <h1 className="text-[128px] md:text-[256px] text-yellow4 font-extrabold">
+        404
+      </h1>
+      <p className="text-2xl mb-8">Page doesn&apos;t exist :((</p>
+    </div>
+  );
+}
